9ee39ba9b8494900e8fae590b0d640d9876c0302
18sasaki <18sasaki@gmail.com>
2013-12-21 20:42:47 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apr2Book::Application.config.secret_token = '5941fff527d7b7fe0b143b384e288c7ddbb7f854dae28c5af25e942b362bd3220bd0af66a527df743e7f5296c1f74bcfc752151ccf48894ba58a2b20b14a0fe7'
